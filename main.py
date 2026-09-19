2093db05fa1eff64570550be36db54a4edd7833e
David <dinoccuenta@gmail.com>
2026-09-19 15:10:34 -0600

    se resolvieron conflictos

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ class Calculator:
         
     def resta(self, a: int, b: int) -> int:
         return a - b
-<<<<<<< HEAD
 
 #prueba issue david
 
 #prueba2 con issue
-=======
 """ CAMBIO NEIDER GUINDIGUA """
->>>>>>> cbc64614c686e7eb7110818cd3e7cc1e05e654ab
